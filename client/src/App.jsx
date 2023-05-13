d919e0d2a33bf26bd6be61aaabbb0e185965910c
chandranshh <chandranshyay@gmail.com>
2023-05-13 20:27:12 +0530

    added registeration page in the frontend -used axios to connect with restapi running in backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import Register from "./components/register/Register";
+import axios from "axios";
 
 function App() {
+  axios.defaults.baseURL = "http://localhost:3001";
+  axios.defaults.withCredentials = true;
+
   return (
     <div>
       <Register />
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
   const setUsernameHandler = (e) => {
     setUsername(e.target.value);
@@ -12,9 +14,36 @@ function Register() {
     setPassword(e.target.value);
   };
 
+  const setEmailHandler = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const submitHandler = async (e) => {
+    try {
+      e.preventDefault();
+
+      await axios.post("api/auth/register", {
+        username: username,
+        password: password,
+        email: email,
+      });
+    } catch (error) {
+      console.log(error.message);
+      console.log(error.config);
+      alert("Error occurred while registering. Please try again later.");
+    }
+  };
+
   return (
     <div className="bg-blue-50 h-screen flex items-center">
-      <form className="w-64 mx-auto mb-12">
+      <form className="w-64 mx-auto mb-12" onSubmit={submitHandler}>
+        <input
+          type="text"
+          placeholder="Email"
+          value={email}
+          onChange={setEmailHandler}
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
         <input
           type="text"
           placeholder="Username"
